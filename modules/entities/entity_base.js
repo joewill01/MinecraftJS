676ca2cd3362b9dc8133d596e2f7d93792226207
winflag10 <53182120+winflag10@users.noreply.github.com>
2020-08-13 16:29:50 +0100

    Added entity

---
+++ modules/entities/entity_base.js
@@ -0,0 +1,10 @@
+class Entity {
+	constructor(hw, hh, el, x, y, z) {
+		this.hitboxWidth = hw;
+		this.hitboxHeight = hh;
+		this.eyeLevel = el;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
---
+++ modules/entities/player.js
@@ -0,0 +1,80 @@
+class Player extends Entity{
+	constructor(){
+		super(0.6, 1.8, 1.7, 0, 100, 0);
+
+		this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		this.camera.position.set(this.x,this.y,this.z);
+		this.controls = new THREE.PointerLockControls(this.camera, renderer.domElement);
+	}
+
+	tp(x,y,z){
+		if(x.toString().includes("~")){
+			if(x.toString().length != 1){
+				this.camera.position.x += parseInt(x.toString().slice(1));
+			}
+		}else{
+			this.camera.position.x = x;
+		}
+
+		if(y.toString().includes("~")){
+			if(y.toString().length != 1){
+				this.camera.position.y += parseInt(y.toString().slice(1));
+			}
+		}else{
+			this.camera.position.y = y;
+		}
+
+		if(z.toString().includes("~")){
+			if(z.toString().length != 1){
+				this.camera.position.z += parseInt(z.toString().slice(1));
+			}
+		}else{
+			this.camera.position.z = z;
+		}
+	}
+
+	moveCamera() {
+
+		var time = performance.now();
+		var delta = ( time - prevTime ) / 1000;
+
+		//Change speed if sprinting
+		if (!sprinting){
+			velocity.x -= velocity.x * 16.0 * delta;
+			velocity.z -= velocity.z * 16.0 * delta;
+		}else{
+			velocity.x -= velocity.x * 10.0 * delta;
+			velocity.z -= velocity.z * 10.0 * delta;
+		}
+		
+		//Change the FOV of the camera to show you are sprinting
+		if(sprinting && this.camera.fov < 80){
+			this.camera.fov ++;
+		}
+		if(!sprinting && this.camera.fov > 70){
+			this.camera.fov -= 0.5;
+		}
+		this.camera.updateProjectionMatrix();
+
+		velocity.y -= 9.8 * 2.5 * delta; // 100.0 = mass
+
+		direction.z = Number( moveForward ) - Number( moveBackward );
+		direction.x = Number( moveRight ) - Number( moveLeft );
+		direction.normalize(); // this ensures consistent movements in all directions
+
+		if ( moveForward || moveBackward ) velocity.z -= direction.z * 1 * delta;
+		if ( moveLeft || moveRight ) velocity.x -= direction.x * 1 * delta;
+
+		this.controls.moveRight( - velocity.x);
+		this.controls.moveForward( - velocity.z);
+
+		// check if we are on the floor level (y = 2 all the time atm)
+		this.controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+		if ( this.controls.getObject().position.y < setYHeight ) {
+			velocity.y = 0;
+			this.controls.getObject().position.y = setYHeight;
+			canJump = true;
+		}
+		prevTime = time;
+	}
+}
