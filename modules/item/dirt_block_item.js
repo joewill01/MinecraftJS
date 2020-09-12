fe8cff29bdfa8c62553a61e4f4c37158a4df631c
winflag10 <53182120+winflag10@users.noreply.github.com>
2020-09-12 09:39:56 +0100

    added dirt item

---
+++ modules/item/dirt_block_item.js
@@ -0,0 +1,23 @@
+class DirtBlockItem extends Item{
+	constructor(){
+		super();
+		this.ID = 12;
+		this.name = "firt_block"; // Item name such as diamond_sword
+		this.displayName = "Dirt Block"; // Same as name but able to be overwritten for custom item names
+		this.type = "block"; // material, tool, food, block, armour
+		this.displayType = "3d"; // 2d/3d 
+
+		this.hasDurability = false;
+
+		//If Block
+		this.blockId = 7;
+		this.blockTextures = {
+			'N': 'dirt.png',
+			'S': 'dirt.png',
+			'E': 'dirt.png',
+			'W': 'dirt.png',
+			'U': 'dirt.png',
+			'D': 'dirt.png',
+		};
+	}
+}
