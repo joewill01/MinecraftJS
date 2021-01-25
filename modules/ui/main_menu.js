18791ae7cd6094c4e4f545d9fedd76a1563b6451
Joseph Williams <williamsj155@cardiff.ac.uk>
2021-01-25 23:11:46 +0000

    fix relative links

--- modules/ui/main_menu.js
+++ modules/ui/main_menu.js
@@ -83,9 +83,9 @@ class MainMenu extends UIBase {
         this.UIelements["buttons"]["quit"] = new UIButton(this.menu_container, 230, 175, 220,"main_menu-quit", "Quit Game");
 
         this.UIelements["images"] = {};
-        this.UIelements["images"]["logo-left"] = new UIImage(this.logo_container, 0, 0, 310, 90  , "main_menu-logo-left", "/minecraft/textures/gui/title/minecraft.png", 512, 512, 0, 0)
-        this.UIelements["images"]["logo-right"] = new UIImage(this.logo_container, 310, 0, 238, 90  , "main_menu-logo-right", "/minecraft/textures/gui/title/minecraft.png", 512, 512, 0, -90)
-        this.UIelements["images"]["edition"] = new UIImage(this.logo_container, 176, 80, 196, 28  , "main_menu-edition", "/minecraft/textures/gui/title/edition.png",32, 256)
+        this.UIelements["images"]["logo-left"] = new UIImage(this.logo_container, 0, 0, 310, 90  , "main_menu-logo-left", "minecraft/textures/gui/title/minecraft.png", 512, 512, 0, 0)
+        this.UIelements["images"]["logo-right"] = new UIImage(this.logo_container, 310, 0, 238, 90  , "main_menu-logo-right", "minecraft/textures/gui/title/minecraft.png", 512, 512, 0, -90)
+        this.UIelements["images"]["edition"] = new UIImage(this.logo_container, 176, 80, 196, 28  , "main_menu-edition", "minecraft/textures/gui/title/edition.png",32, 256)
 
         this.UIelements["texts"] = {};
         this.UIelements["texts"]["splash"] = new UIFlashyYellowText(this.logo_container, 380, 50, "main_menu-logo-splash", "Spiders everywhere!",)
