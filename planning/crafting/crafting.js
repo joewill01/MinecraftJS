d080cc88222e13af382189f24df17c1612a00801
benjamesmcdonald <benjmcdonald2002@gmail.com>
2025-02-13 19:57:08 +0000

    basic shaped crafting

--- planning/crafting/crafting.js
+++ planning/crafting/crafting.js
@@ -1,4 +1,4 @@
-testRecipes = {
+testRecipes = [{
   "type": "minecraft:crafting_shaped",
   "category": "misc",
   "group": "bed",
@@ -14,10 +14,32 @@ testRecipes = {
     "count": 1,
     "id": "minecraft:white_bed"
   }
-}
-
-let craftingGrid = [[" "," "," "],[" "," "," "],[" "," "," "]]
-reformatRecipe(testRecipes)
+},
+{
+  "type": "minecraft:crafting_shaped",
+  "category": "misc",
+  "group": "wooden_fence",
+  "key": {
+    "#": "minecraft:stick",
+    "W": "minecraft:acacia_planks"
+  },
+  "pattern": [
+    "W#W",
+    "W#W"
+  ],
+  "result": {
+    "count": 3,
+    "id": "minecraft:acacia_fence"
+  }
+}]
+let reformattedRecipes = reformatAll(testRecipes)
+// let craftingGrid = [[" "," "," "],[" ","x"," "],[" ","p"," "]]
+let craftingGrid = [
+  [" ", " ", " "],
+  ['minecraft:white_wool','minecraft:white_wool','minecraft:white_wool'],
+  [ '#minecraft:planks', '#minecraft:planks', '#minecraft:planks' ]
+]
+console.log(shapedCraft(craftingGrid, reformattedRecipes))
 
 function addItem(item, position){
     vertical = Math.floor(position/3)
@@ -25,20 +47,22 @@ function addItem(item, position){
     craftingGrid[vertical][horizontal] = item
 }
 
-
 function reformatRecipe(recipe){
     let pattern = recipe['pattern']
-    let reformattedRecipe = pattern.map(item => [...item]);
     let key = recipe['key']
-    for (let i = 0; i < reformattedRecipe.length; i++) {
-        let row = reformattedRecipe[i]
-        for (let j = 0; j < row.length; j++) {
-            reformattedRecipe[i][j] = key[reformattedRecipe[i][j]]
-            
-        }
-        
-    }
+    let reformattedRecipe = pattern.map(item => [...item]);
+    return reformattedRecipe.map(row => row.map(cell => key[cell]));
+}
 
+function reformatAll(unformattedRecipes){
+  return unformattedRecipes.map(recipe => {
+    let { key, ...newRecipe } = recipe;
+    return { ...newRecipe, pattern: reformatRecipe(recipe) };
+  });
+}
 
-    console.log(reformattedRecipe)
+function shapedCraft(craftingGrid, recipes){
+  craftingGrid = craftingGrid.filter(row => JSON.stringify(row) !== JSON.stringify([" ", " ", " "]));
+  filteredArray = recipes.find(obj => JSON.stringify(obj['pattern']) === JSON.stringify(craftingGrid));
+  return filteredArray.result
 }
